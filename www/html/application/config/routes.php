c356149c6f74f932214d8237320f0a1304233902
Tsuyoshi Kawasaki <sambatriste@gmail.com>
2020-05-01 21:20:39 +0900

    add news posting.

--- www/html/application/config/routes.php
+++ www/html/application/config/routes.php
@@ -53,5 +53,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 //$route['404_override'] = '';
 //$route['translate_uri_dashes'] = FALSE;
 
-$route['default_controller'] = 'pages/view';
+$route['news/create'] = 'news/create';
+$route['news/(:any)'] = 'news/view/$1';
+$route['news'] = 'news';
 $route['(:any)'] = 'pages/view/$1';
+$route['default_controller'] = 'pages/view';
